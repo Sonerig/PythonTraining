3c975b65d373f3017ece8b3abe05209fbdb6fd6b
Sonerig <77194357+Sonerig@users.noreply.github.com>
2024-08-15 11:26:08 +0300

    .

--- StringMethods&ProgramOrganization/module_1_4.py
+++ StringMethods&ProgramOrganization/module_1_4.py
@@ -1,7 +1,7 @@
-my_string = input("Введите текст: ")
-print(f"Количество введенных символов: {my_string.__len__()}")
-print(my_string.upper())                    # Вывод строки в верхнем регистре
-print(my_string.lower())                    # Вывод строки в нижнем регистре
-my_string = my_string.replace(" ", "")      # Удаление пробелов в строке
-print(my_string[0])                         # Вывод первого символа
-print(my_string[my_string.__len__() - 1])   # Вывод последнего символа
+my_string = input("Введите текст: ")                            # Ввод Текста
+print(f"Количество введенных символов: {my_string.__len__()}")  # Вывод количества символов
+print(my_string.upper())                                        # Вывод строки в верхнем регистре
+print(my_string.lower())                                        # Вывод строки в нижнем регистре
+my_string = my_string.replace(" ", "")                          # Удаление пробелов в строке
+print(my_string[0])                                             # Вывод первого символа
+print(my_string[my_string.__len__() - 1])                       # Вывод последнего символа
