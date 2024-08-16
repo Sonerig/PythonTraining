8a44808260a3a5a65880c2d57a97fdc30ee54e67
Sonerig <77194357+Sonerig@users.noreply.github.com>
2024-08-16 14:35:17 +0300

    Update module_1_4.py

--- StringMethods&ProgramOrganization/module_1_4.py
+++ StringMethods&ProgramOrganization/module_1_4.py
@@ -3,5 +3,6 @@ print(f"Количество введенных символов: {my_string.__l
 print(my_string.upper())                                        # Вывод строки в верхнем регистре
 print(my_string.lower())                                        # Вывод строки в нижнем регистре
 my_string = my_string.replace(" ", "")                          # Удаление пробелов в строке
+print(my_string)                                                # Вывод строки после удаления пробелов в ней
 print(my_string[0])                                             # Вывод первого символа
 print(my_string[my_string.__len__() - 1])                       # Вывод последнего символа
