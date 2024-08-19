f9e08445e4b2e7ecd737fadf5d4eb114fb6688fb
Sonerig <kambachokovidar@gmail.com>
2024-08-19 15:54:11 +0300

    .

--- ExtraPracticeTask/main.py
+++ ExtraPracticeTask/main.py
@@ -2,8 +2,7 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 # Перевод множества в список и его сортировка
-students = list(students)
-students.sort()
+students = list(sorted(students))
 # Создание словаря
 DictOfStudentsAVGGrades = dict()
 # Рассчет среднего балла и добавление студента с оценкой в словарь
