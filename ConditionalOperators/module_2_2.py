2160267866289a257fe7c2a687a2d72558418760
Sonerig <kambachokovidar@gmail.com>
2024-08-19 16:22:06 +0300

    .

---
+++ ConditionalOperators/module_2_2.py
@@ -0,0 +1,10 @@
+first = input("First num: ")
+second = input("Second num: ")
+third = input("Third num: ")
+
+if first == second and first == third and second == third:
+    print("3")
+elif first == second or first == third or second == third:
+    print("2")
+else:
+    print("0")
