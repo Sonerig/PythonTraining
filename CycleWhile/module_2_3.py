f050e37469fb904d726f88a57dd56d1998aed669
Sonerig <kambachokovidar@gmail.com>
2024-08-20 23:53:35 +0300

    .

--- CycleWhile/module_2_3.py
+++ CycleWhile/module_2_3.py
@@ -1,7 +1,8 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-iteration = 0
-while True:
-    if my_list[iteration] < 0 or iteration == len(my_list):
-        break
-    print(my_list[iteration])
-    iteration += 1
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]  # Исходный список
+index = 0                                               # Индексация списка
+
+while True:                                             # Бесконечный цикл
+    if my_list[index] < 0 or index == len(my_list):     # Условие выхода из цикла
+        break                                           # Выход из цикла при выполнении условия
+    print(my_list[index])                               # Вывод текущего элемента
+    index += 1                                          # Инкрементация индекса
