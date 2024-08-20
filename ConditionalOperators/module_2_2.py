c74317e744a1abb3def7b556e351c5abe3c02842
Sonerig <kambachokovidar@gmail.com>
2024-08-20 23:15:33 +0300

    .

--- ConditionalOperators/module_2_2.py
+++ ConditionalOperators/module_2_2.py
@@ -1,10 +1,10 @@
-first = input("First num: ")
-second = input("Second num: ")
-third = input("Third num: ")
+first = input("First num: ")    # Ввод первого числа
+second = input("Second num: ")  # Ввод второго числа
+third = input("Third num: ")    # Ввод третьего числа
 
-if first == second and first == third and second == third:
-    print("3")
+if first == second and first == third:
+    print("3")  # Вывод при условии что все числа равны
 elif first == second or first == third or second == third:
-    print("2")
+    print("2")  # Вывод при условии что хотя бы 2 чисела равны
 else:
-    print("0")
+    print("0")  # Вывод при условии что равных чисел нет
