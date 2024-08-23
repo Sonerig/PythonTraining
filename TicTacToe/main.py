8135d8fb420410bf2cd62026de6f52309e8da41e
Sonerig <kambachokovidar@gmail.com>
2024-08-24 01:58:13 +0300

    Comments added

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,4 +1,4 @@
-def draw_area(area):
+def draw_area(area):                                                    # Функция отрисовки в консоль доски
     print("-" * 13)
     for i in range(len(area)):
         print("|", area[i][0], "|", area[i][1], "|", area[i][2], "|")
@@ -6,87 +6,91 @@ def draw_area(area):
     print("=" * 56)
 
 
-def check_if_win(area, current_turn):
+def check_if_win(area, current_turn):                                       # Функция проверки на победителя
 
-    for row_column in range(3):
+    for row_column in range(3):                                             # Проверка по главной диагонали
         if area[row_column][row_column] != current_turn:
             break
         elif row_column == len(area) - 1:
             return True
 
-    for row_column in range(3):
+    for row_column in range(3):                                             # Проверка по обратной диагонали
         if area[row_column][len(area) - row_column - 1] != current_turn:
             break
         elif row_column == len(area) - 1:
             return True
 
-    for row in range(3):
+    for row in range(3):                                                    # Проверка по строкам
         for column in range(3):
             if area[row][column] != current_turn:
                 break
             elif column == len(area) - 1:
                 return True
 
-    for column in range(3):
+    for column in range(3):                                                 # Проверка по столбцам
         for row in range(3):
             if area[row][column] != current_turn:
                 break
             elif row == len(area) - 1:
                 return True
 
-    return False
+    return False                                                            # Вернуть ложь если нет победителя
 
 
-def update_area(area, turn):
-    try:
+def set_in_area(area, turn):                                                # Ход игрока
+    try:                                                                    # Ввод строки и столбца
         row = int(input("Enter the row: ")) - 1
         column = int(input("Enter the column: ")) - 1
-    except ValueError:
+    except ValueError:                                                      # Обработка исключения если введено не число
         print("Invalid number (you can use: 1, 2, 3)")
         print("=" * 56)
-        return update_area(area, turn)
-    if (row < 0 or row >= 3) or (column < 0 or column >= 3):
+        return set_in_area(area, turn)
+
+    if (row < 0 or row >= 3) or (column < 0 or column >= 3):                # Обработка если число больше 3 или меньше 1
         print("The row or column can not be more than 3 and less than 1")
         print("=" * 56)
-        return update_area(area, turn)
-    if area[row][column] != '#':
+        return set_in_area(area, turn)
+
+    if area[row][column] != '#':                                            # Обработка если ячейка уже занята
         print("This cell is already busy")
         print("=" * 56)
-        return update_area(area, turn)
+        return set_in_area(area, turn)
 
-    area[row][column] = turn
+    area[row][column] = turn                                                # Постваить ход игрока
     print("=" * 56)
 
 
-def main():
-    print("Wellcome to Tic-Tak game!")
+def main():                                                         # Главная функция
+    print("Wellcome to Tic-Tak game!")                              # Приветствие
     print("=" * 56)
+    area = [['#', '#', '#'], ['#', '#', '#'], ['#', '#', '#']]      # Поле игры
+
     try:
-        area = [['#', '#', '#'], ['#', '#', '#'], ['#', '#', '#']]
-        draw_area(area)
+        draw_area(area)                     # Отрисовать поле игры
+
         for turn in range(9):
-            if turn % 2 == 0:
+            if turn % 2 == 0:               # Ход крестика
                 print("X turn!")
                 print("=" * 56)
-                update_area(area, 'X')
-            else:
+                set_in_area(area, 'X')
+            else:                           # Ход нолика
                 print("0 turn!")
                 print("=" * 56)
-                update_area(area, '0')
+                set_in_area(area, '0')
 
-            draw_area(area)
+            draw_area(area)                 # Отрисовать поле игры
 
-            if check_if_win(area, "X"):
+            if check_if_win(area, "X"):     # Проверка крестика на победителя
                 print("X win!")
                 print("=" * 56)
                 return
-            elif check_if_win(area, "0"):
+            elif check_if_win(area, "0"):   # Проверка нолика на победителя
                 print("0 win!")
                 print("=" * 56)
                 return
-    except KeyboardInterrupt:
+    except KeyboardInterrupt:               # Обработка исключения при досрочном выходе из игры
         print("\nBye-bye")
-    print("Game over")
+    print("Game over")                      # Ничья
 
 
 main()
