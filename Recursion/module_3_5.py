d6e0406222645cba29112de2fd8c036a1c90f140
Sonerig <kambachokovidar@gmail.com>
2024-09-01 21:50:12 +0300

    Added comments

--- Recursion/module_3_5.py
+++ Recursion/module_3_5.py
@@ -1,8 +1,8 @@
-def get_multiplied_number(number):
-    number = str(number).replace('0', '')
+def get_multiplied_number(number):                                          # Функция умножения цифр в числе
+    number = str(number).replace('0', '')                                   # Удаление нулей в полученом числе
     if len(number) > 1:
-        return int(number[0]) * get_multiplied_number(int(number[1:]))
-    return int(number[0])
+        return int(number[0]) * get_multiplied_number(int(number[1:]))      # Рекурсия, если цифр больше чем 1
+    return int(number[0])                                                   # Возврат последнего числа
 
 
-print(get_multiplied_number(int(input("The num: "))))
+print(get_multiplied_number(int(input("The num: "))))                       # Вывод итога, вызов функции, ввод числа
