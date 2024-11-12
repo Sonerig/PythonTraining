78aac1bcfbc31ae42806692db7cfe4c74e8c888a
Sonerig <kambachokovidar@gmail.com>
2024-11-12 23:16:50 +0300

    Module 10.5 task

---
+++ Module_10/Multiprocessing/module_10_5.py
@@ -0,0 +1,25 @@
+import time
+import multiprocessing
+
+
+def read_info(name):
+    all_data = list()
+    with open(name, 'r', encoding="utf-8") as file:
+        all_data.append(file.readline())
+
+
+file_names = [f"file {num}.txt" for num in range(1, 5)]
+
+if __name__ == "__main__":
+    first_timer = time.time()
+    for file_name in file_names:
+        read_info(file_name)
+    first_timer_result = time.time() - first_timer
+
+    second_timer = time.time()
+    with multiprocessing.Pool(4) as pool:
+        pool.map(read_info, file_names)
+    second_timer_result = time.time() - second_timer
+
+    print(f"Линейный: {first_timer_result}\n"
+          f"Многопроцессный: {second_timer_result}")
