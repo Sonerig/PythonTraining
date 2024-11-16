9c1e18c60b1a2d515ffebcada407c2fc88da2b40
Sonerig <77194357+Sonerig@users.noreply.github.com>
2024-11-17 02:07:43 +0300

    Update module_11_2.py

--- Module_11/Introspection/module_11_2.py
+++ Module_11/Introspection/module_11_2.py
@@ -12,7 +12,7 @@ class Temp:
 
 def introspection_info(obj):
     return {"Тип объекта: ": type(obj),
-            "Атрибуты обекта: ": dir(obj),
+            "Атрибуты обекта: ": [attr_name for attr_name in dir(obj) if not callable(getattr(obj, attr_name))],
             "Методы объекта: ": [attr_name for attr_name in dir(obj) if callable(getattr(obj, attr_name))],
             "Модуль к которому принадлежит объект: ": inspect.getmodule(obj)}
 
