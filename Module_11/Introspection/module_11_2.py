cd6a8290e0edb397238d79d8686a52548a11b054
Sonerig <kambachokovidar@gmail.com>
2024-11-16 15:47:41 +0300

    Module 11.2 task

---
+++ Module_11/Introspection/module_11_2.py
@@ -0,0 +1,21 @@
+from pprint import pprint
+import inspect
+
+
+class Temp:
+    def __init__(self):
+        self.a = "temp"
+
+    def foo(self):
+        return self.a
+
+
+def introspection_info(obj):
+    return {"Тип объекта: ": type(obj),
+            "Атрибуты обекта: ": dir(obj),
+            "Методы объекта: ": [attr_name for attr_name in dir(obj) if callable(getattr(obj, attr_name))],
+            "Модуль к которому принадлежит объект: ": inspect.getmodule(obj)}
+
+
+b = Temp()
+pprint(introspection_info(b))
