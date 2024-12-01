161d4712490d0ae789faa531cbafe07e3d884f53
Sonerig <kambachokovidar@gmail.com>
2024-12-01 14:41:28 +0300

    Module 12.1 task

---
+++ Module_12/SimpleUnitTests/tests_12_1.py
@@ -0,0 +1,24 @@
+from runner import Runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = Runner("Alex")
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner("Alex")
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner("Alex")
+        runner2 = Runner("Steve")
+        for _ in range(10):
+            runner1.run()
+            runner2.walk()
+        self.assertNotEqual(runner1.distance, runner2.distance)
