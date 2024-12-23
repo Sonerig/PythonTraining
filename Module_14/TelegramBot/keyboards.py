9f8d2daee74440cf672eb9201fee4345fb0dbbab
Sonerig <kambachokovidar@gmail.com>
2024-12-23 19:37:01 +0300

    TG-Bots keyboards

---
+++ Module_14/TelegramBot/keyboards.py
@@ -0,0 +1,37 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+main_kb = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    keyboard=[
+        [
+            KeyboardButton(text="Рассчитать"),
+            KeyboardButton(text="Информация")
+        ],
+        [
+            KeyboardButton(text="Купить")
+        ]
+    ]
+)
+
+inline_calculate_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Формулы расчёта", callback_data='formulas'),
+            InlineKeyboardButton(text="Рассчитать норму калорий", callback_data="calories")
+        ]
+    ]
+)
+
+inline_buying_kb = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Product1", callback_data="product_buying"),
+            InlineKeyboardButton(text="Product2", callback_data="product_buying")
+        ],
+        [
+            InlineKeyboardButton(text="Product3", callback_data="product_buying"),
+            InlineKeyboardButton(text="Product4", callback_data="product_buying")
+        ]
+    ]
+)
