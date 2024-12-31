1ad093048240601c9d59275fc7c88f1820b19ec3
Sonerig <kambachokovidar@gmail.com>
2024-12-31 15:24:58 +0300

    State classes

---
+++ Module_14/SimpleORM/StateClasses.py
@@ -0,0 +1,12 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
