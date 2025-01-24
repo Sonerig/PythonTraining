023b7beb98fa6e1490d62a51d6d88107b37fd3c2
Sonerig <kambachokovidar@gmail.com>
2025-01-24 15:59:44 +0300

    Module 16.2 task

--- Module_16/Validation/module_16_2.py
+++ Module_16/Validation/module_16_2.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -11,9 +12,10 @@ async def admin() -> str:
     return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async def user(user_id: int) -> str:
+async def user(user_id: Annotated[int, Path(ge=1, le=100, description="Enter user ID", example="1")]) -> str:
     return f"Вы вошли как пользователь № {user_id}"
 
-@app.get("/user")
-async def username_age(username: str, age: int) -> str:
+@app.get("/user/{username}/{age}")
+async def username_age(username: Annotated[str, Path(min_length=10, max_length=20, description="Enter username", example="UrbanUser")],
+                       age: Annotated[int, Path(ge=18, le=120, description="Enter age", example='24')]) -> str:
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
